97bcca93b05352cf868b44c2e94f0b8a0e4e3b44
roy-edwin-ics4u1-03-25 <roy.edwin@student.tdsb.on.ca>
2025-02-06 16:11:31 -0500

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,37 +13,72 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		String yippie = in.nextLine();
+		System.out.println(yippie.charAt(0));
+		
+
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		boolean notboo = in.nextBoolean();
+		in.nextLine();
+		System.out.println(!notboo);
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		int greaterthan5 = in.nextInt();
+		in.nextLine();
+		System.out.println(greaterthan5 > 5);
 		
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		double two = in.nextDouble();
+		in.nextLine();
+		System.out.println(two >= -2 && two <= 2 );
+
 		
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		String helloworld = in.nextLine();
+		System.out.println(helloworld.equals("Hello World"));
 		
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		int firstimput = in.nextInt();
+		in.nextLine();
+		System.out.print("In: ");
+		int secondimput = in.nextInt();
+		in.nextLine();
+		System.out.println(firstimput <= secondimput);
 		
 	}
 
 	public static void q7() {
-		//Write question 7 code here
+		Scanner in = new Scanner(System.in);
+		System.out.print("In: ");
+		double firstdouble = in.nextDouble();
+		in.nextLine();
+		System.out.print("In: ");
+		double seconddouble = in.nextDouble();
+		in.nextLine();
+		System.out.println(firstdouble > seconddouble);
 		
 	}
 
